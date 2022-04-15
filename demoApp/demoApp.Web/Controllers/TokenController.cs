01ca05e7ad72a1805746c79cfd728c81bc329d2b
Jawad Shani <jawad.shani@hitachi-eu.com>
2022-04-15 21:59:10 +0200

    token controller added

---
+++ demoApp/demoApp.Web/Controllers/TokenController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace demoApp.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        public dynamic Get()
+        {
+            return new
+            {
+                Guid = Guid.NewGuid().ToString(),
+                Expires = DateTime.Now.AddDays(1),
+                Issuer = Environment.MachineName
+            };
+        }
+    }
+}
