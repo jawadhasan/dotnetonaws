d7e3e169624983bbea92557c16cea42df0459f5e
Jawad Shani <jawad.shani@hitachi-eu.com>
2024-07-30 12:25:32 +0200

    connection update and sql script added

---
+++ demoApp/demoApp.Data/add-users-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO users(email, firstName, lastName)
+VALUES
+('walter.white@breakingbad.com','Walter', 'White'),
+('jessy.pinkman@breakingbad.com','Jessy', 'Pinkman'),
+('hecktor.salmanca@breakingbad.com','Hecktor', 'Salmanca'),
+('nacho.varga@breakingbad.com','Nacho', 'Varga'),
+('walter.white@breakingbad.com','Walter', 'White'),
+('tuco.salmanca@breakingbad.com','Tuco', 'Salmanca'),
+
---
+++ demoApp/demoApp.Data/create-users-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL, 
+  email VARCHAR(255) UNIQUE NOT NULL,
+  firstName VARCHAR(50),
+  lastName VARCHAR(50),
+  createdat TIMESTAMPTZ DEFAULT NOW()
+  );
